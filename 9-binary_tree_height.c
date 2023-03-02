aa99ca590ce2e48ab11be5447d6f29c9e66cb0aa
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-03-02 07:42:39 -0800

    done

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,6 +10,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
+	if (tree != NULL)
+	{
+		if (tree->left == NULL && tree->right == NULL)
+			return (0);
+	}
 	size_t lDepth = binary_tree_height(tree->left);
 	size_t rDepth = binary_tree_height(tree->right);
 
