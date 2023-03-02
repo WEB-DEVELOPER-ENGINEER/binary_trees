f0278bfbc2a6b90ece804c91959d2b80859834ff
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-03-02 07:50:47 -0800

    done

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (tree != NULL)
+	else if (tree != NULL)
 	{
 		if (tree->left == NULL && tree->right == NULL)
 			return (0);
