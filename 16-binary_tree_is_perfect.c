3f7f7c1c0ab099caf2425598d8945918a234ee11
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-03-02 16:07:30 -0800

    done

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -9,7 +9,7 @@ size_t binary_tree_height(const binary_tree_t *tree);
  * @tree: pointer to the tree
  * Return: number of leaves
  */
-size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 if (tree == NULL)
 	return (0);
